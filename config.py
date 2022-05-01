3708cd2aa35b766e316f889f31c24231fc39d0b2
GuizaZeineb <45284041+GuizaZeineb@users.noreply.github.com>
2022-05-01 14:00:22 +0200

    Update config.py

--- config.py
+++ config.py
@@ -34,8 +34,9 @@ class DefaultConfig:
 
         #-------details for github action______
         self.PORT = 8000
-        self.APP_ID = os.environ.get("CHATBOT_BOT_ID", "3c699fe5-4776-47a0-93ca-baa0258c7958") #"this one"
-        self.APP_PASSWORD = os.environ.get("CHATBOT_BOT_PASSWORD", "sjD8Q~pzxGQ3FQHil2TLW9Fg6Gsg.zRW2vA.Ydqs")#self.CHATBOT_BOT_ID = os.environ.get("CHATBOT_BOT_ID", "3c699fe5-4776-47a0-93ca-baa0258c7958")
+        self.APP_ID = os.environ.get("MicrosoftAppId", "3c699fe5-4776-47a0-93ca-baa0258c7958") #"this one"
+        self.APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "sjD8Q~pzxGQ3FQHil2TLW9Fg6Gsg.zRW2vA.Ydqs")
+#        self.CHATBOT_BOT_ID = os.environ.get("CHATBOT_BOT_ID", "3c699fe5-4776-47a0-93ca-baa0258c7958")
 #        self.CHATBOT_BOT_PASSWORD = os.environ.get("CHATBOT_BOT_PASSWORD", "sjD8Q~pzxGQ3FQHil2TLW9Fg6Gsg.zRW2vA.Ydqs") #"This one"
         self.APP_TYPE = os.environ.get("MicrosoftAppType", "MultiTenant")
 
