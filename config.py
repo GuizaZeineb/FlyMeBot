6c17364700537e553c099481cf493b552d637470
GuizaZeineb <45284041+GuizaZeineb@users.noreply.github.com>
2022-05-01 18:23:33 +0200

    add credentials

--- config.py
+++ config.py
@@ -7,8 +7,13 @@ from dotenv import load_dotenv
 
 class DefaultConfig:
     """ Bot Configuration """
-    load_dotenv()
-
-    PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+        def __init__(self):
+        load_dotenv()
+        self.PORT = 8000#3978
+        #_____Soit Cela
+        self.APP_ID = os.environ.get("MicrosoftAppId", "3c699fe5-4776-47a0-93ca-baa0258c7958") #"this one"
+        self.APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "sjD8Q~pzxGQ3FQHil2TLW9Fg6Gsg.zRW2vA.Ydqs")
+        #_____Soit Cela
+#        self.CHATBOT_BOT_ID = os.environ.get("CHATBOT_BOT_ID", "3c699fe5-4776-47a0-93ca-baa0258c7958")
+#        self.CHATBOT_BOT_PASSWORD = os.environ.get("CHATBOT_BOT_PASSWORD", "sjD8Q~pzxGQ3FQHil2TLW9Fg6Gsg.zRW2vA.Ydqs") #"This one"
+#        self.APP_TYPE = os.environ.get("MicrosoftAppType", "MultiTenant")
