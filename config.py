c5d20b8d0362dbe9dc862a76feba310da7336088
GuizaZeineb <45284041+GuizaZeineb@users.noreply.github.com>
2022-05-01 17:34:21 +0200

    remove all the config data and put it statically

--- config.py
+++ config.py
@@ -34,11 +34,13 @@ class DefaultConfig:
 
         #-------details for github action______
         self.PORT = 8000
-        self.APP_ID = os.environ.get("MicrosoftAppId", "3c699fe5-4776-47a0-93ca-baa0258c7958") #"this one"
-        self.APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "sjD8Q~pzxGQ3FQHil2TLW9Fg6Gsg.zRW2vA.Ydqs")
+        #_____Soit Cela
+#        self.APP_ID = os.environ.get("MicrosoftAppId", "3c699fe5-4776-47a0-93ca-baa0258c7958") #"this one"
+#        self.APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "sjD8Q~pzxGQ3FQHil2TLW9Fg6Gsg.zRW2vA.Ydqs")
+        #_____Soit Cela
 #        self.CHATBOT_BOT_ID = os.environ.get("CHATBOT_BOT_ID", "3c699fe5-4776-47a0-93ca-baa0258c7958")
 #        self.CHATBOT_BOT_PASSWORD = os.environ.get("CHATBOT_BOT_PASSWORD", "sjD8Q~pzxGQ3FQHil2TLW9Fg6Gsg.zRW2vA.Ydqs") #"This one"
-        self.APP_TYPE = os.environ.get("MicrosoftAppType", "MultiTenant")
+#        self.APP_TYPE = os.environ.get("MicrosoftAppType", "MultiTenant")
 
         #_______________Nouveau Luis____________
     #    LUIS_APP_ID = os.environ.get("LuisAppId", "e2c9e12a-31f4-4956-a89f-cc3fd548865d") # Random
